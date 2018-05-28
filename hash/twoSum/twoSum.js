591c136f2f877316ff57225a722ac553e83d036f
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-28 09:44:10 +0200

    first commit

---
+++ hash/twoSum/twoSum.js
@@ -0,0 +1,28 @@
+const twoSum = function( nums, target) {
+
+  /* array to return the result */
+  let arr = []
+
+  /* map for num and index pair */
+/*  Map<integer, integer=""> map =
+      new HashMap<integer, integer="">();
+/*
+  for (let i = 0; i < nums.length; i++) {
+
+
+    Integer val = map.get(target
+        - nums[i]);
+    if (val == null) {
+
+      map.put(nums[i], i);
+    } else {
+
+      arr[0] = val;
+      arr[1] = i;
+      break; // exit the loop
+    }
+  }*/
+  return 'yeah';
+}
+
+module.exports = twoSum;
---
+++ hash/twoSum/twoSum.test.js
@@ -0,0 +1,8 @@
+const expect = require('expect');
+const twoSum = require('./twoSum');
+
+describe('TwoSum problem', () =>{
+  it('should return', () => {
+      expect(twoSum()).toBe('yeah');
+  })
+})
