142b02bd5c01097397e180c736c8332509572450
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-29 17:36:16 +0200

    Added tests

--- src/data_structures/linked_list/linked_list.js
+++ src/data_structures/linked_list/linked_list.js
@@ -1,7 +1,20 @@
 
 class LinkedList {
+	constructor (initial = []) {
+		this._elements = initial; 
+	}
 
+	size () {
+		return this._elements.length;
+	}
 
+	add(element) {
+		this._elements.push(element);
+	}
+
+	find(element) {
+		return this._elements.indexOf(element);
+	}
 }
 
 
--- src/data_structures/linked_list/linked_list.test.js
+++ src/data_structures/linked_list/linked_list.test.js
@@ -2,10 +2,29 @@ const chai = require('chai');
 const expect = chai.expect;
 const LinkedList = require('./linked_list');
 
+let linkedList;
+
 describe('Testing Linked List structure', () => {
 
-it('should exist', () => {
+	beforeEach(() => {
+		linkedList = new LinkedList();		
+	});
 
-});
+	it('should exist', () => {
+		expect(LinkedList).to.exist;	
+	});
+	
+	it('should be empty', () => {
+		expect(linkedList.size()).to.equal(0);	
+	});
 
+	it('should add correctly', () => {
+		linkedList.add(666);
+		expect(linkedList.size()).to.equal(1);
+	});
+
+	it('should find existing element', () => {
+		linkedList = new LinkedList([88,42,15,666]);
+		expect(linkedList.find(15)).to.equal(2);
+	});
 });
