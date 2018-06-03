808d862ef3c4aa344ae2f52fb0ae146803cf261d
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-03 21:42:52 +0200

    tests corrected

--- src/math/reverse_integer/reverse_integer.js
+++ src/math/reverse_integer/reverse_integer.js
@@ -1,6 +1,13 @@
 const reverseInteger = function(number) {
+	let result = 0;
 
-	return number;
+    while (number != 0) {
+      result = (result * 10) + (number % 10);
+      number = Math.floor(number/10);
+    }
+
+    return (result > Number.MAX_VALUE || 
+            result < Number.MIN_VALUE ? 0 : result);
 }
 
 module.exports = reverseInteger;
--- src/math/reverse_integer/reverse_integer.test.js
+++ src/math/reverse_integer/reverse_integer.test.js
@@ -1,8 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
+const reverseInteger = require('./reverse_integer');
 
 describe('math/reverse_integer', () => {
 	it('should reverse an integer', () => {
-
+		expect(reverseInteger(12345)).to.equal(54321);
 	});
 });
